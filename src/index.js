ded7f2daf9c8ed0503254b7b9c826d72dd0ce2d9
tangjingfu <jingfu.tang@keepsoft.net>
2022-02-25 16:54:54 +0800

    chore: 支持css打包 添加 style-loader, css-loader 配置 webpack 打包 css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import _ from "lodash";
+import _ from 'lodash';
+import './style.css'
 
 function component() {
   var element = document.createElement('div');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello')
 
   return element;
 }
