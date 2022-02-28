ffc6e2dd4523b4dbc7246642c1c068d337ddafab
tangjingfu <jingfu.tang@keepsoft.net>
2022-02-28 10:38:02 +0800

    chore: 恢复更改

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png'
 
 function component() {
   const element = document.createElement('div');
@@ -9,9 +7,6 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello')
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon)
 
   return element;
 }
