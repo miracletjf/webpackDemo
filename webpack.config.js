2540c9761595d5d3ebd86e036406ab5fa6af747d
tangjingfu <jingfu.tang@keepsoft.net>
2022-02-28 16:20:48 +0800

    chore: 添加 html-webpack-plugin 插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    print: './src/print.js',
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management"
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
